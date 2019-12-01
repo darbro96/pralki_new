5ba9c88b0ce4186e23ff488cb467a4948342ce88
darbro96 <dbronikowsk@gmail.com>
2019-12-01 20:11:23 +0100

    Poprawki ObjectInDatabaseException

--- src/main/java/pl/utp/edu/pralki/exceptions/ObjectInDatabaseException.java
+++ src/main/java/pl/utp/edu/pralki/exceptions/ObjectInDatabaseException.java
@@ -1,9 +1,10 @@
 package pl.utp.edu.pralki.exceptions;
 
+import java.io.Serializable;
 import java.util.logging.Logger;
 
-public class ObjectInDatabaseException extends Exception {
-    private final Logger log = Logger.getLogger(getClass().getName());
+public class ObjectInDatabaseException extends Exception implements Serializable {
+    private static final Logger log = Logger.getLogger(ObjectInDatabaseException.class.getName());
 
     public ObjectInDatabaseException() {
         log.info("Obiekt o podanych parametrach jest już w bazie danych!");
